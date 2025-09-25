bec0e5cb9502abd62ae22a6be45d11c014adfc0e
philipfan <philipfan@fireshark.tech>
2025-09-25 23:23:09 +0800

    Refactor: Relocate Staff and UserToken models
    - Move `Staff` data class from `UserToken.kt` to `Staff.kt`.
    - Move `UserToken` data class from `Staff.kt` to `UserToken.kt`.

--- app/src/main/java/com/example/staffmanagementapp/data/model/domain/Staff.kt
+++ app/src/main/java/com/example/staffmanagementapp/data/model/domain/Staff.kt
@@ -1,11 +1,24 @@
+
 package com.example.staffmanagementapp.data.model.domain
 
 /**
- * Domain model representing a user authentication token.
- * This is the Token object used in the app's business logic layer.
+ * Represents a single staff member in the domain model.
+ * This is the staff object used by the app in the UI layer and business logic layer.
+ * It is converted from the StaffDto in the data layer.
  *
- * @property value The string value of the token.
+ * The advantage of this design is that even if the API's DTO format changes in the future,
+ * as long as the conversion logic is updated, the UI and ViewModel will not be affected.
+ *
+ * @property id The unique ID of the staff member.
+ * @property email The email address of the staff member.
+ * @property fullName The full name of the staff member (concatenated from firstName and lastName).
+ * @property avatarUrl The URL of the staff member's avatar image.
  */
-data class UserToken(
-    val value: String
+data class Staff(
+    val id: Int,
+    val email: String,
+    val fullName: String,
+
+    // Property name changed from avatar to avatarUrl to more clearly express that it is a URL
+    val avatarUrl: String
 )
--- app/src/main/java/com/example/staffmanagementapp/data/model/domain/UserToken.kt
+++ app/src/main/java/com/example/staffmanagementapp/data/model/domain/UserToken.kt
@@ -1,23 +1,11 @@
 package com.example.staffmanagementapp.data.model.domain
 
 /**
- * Represents a single staff member in the domain model.
- * This is the staff object used by the app in the UI layer and business logic layer.
- * It is converted from the StaffDto in the data layer.
+ * Domain model representing a user authentication token.
+ * This is the Token object used in the app's business logic layer.
  *
- * The advantage of this design is that even if the API's DTO format changes in the future,
- * as long as the conversion logic is updated, the UI and ViewModel will not be affected.
- *
- * @property id The unique ID of the staff member.
- * @property email The email address of the staff member.
- * @property fullName The full name of the staff member (concatenated from firstName and lastName).
- * @property avatarUrl The URL of the staff member's avatar image.
+ * @property value The string value of the token.
  */
-data class Staff(
-    val id: Int,
-    val email: String,
-    val fullName: String,
-
-    // Property name changed from avatar to avatarUrl to more clearly express that it is a URL
-    val avatarUrl: String
+data class UserToken(
+    val value: String
 )
